f5274d0e64afe65410d3cfb9cd6ab25530375147
Austin Rooks <Austionian@users.noreply.github.com>
2023-02-18 19:43:05 -0600

    refactor day 3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,44 +1,47 @@
 use anyhow::Result;
 
-fn tree_counter(path: &str, travses: Vec<(usize, usize)>) -> Result<usize> {
+fn tree_counter(input: &Vec<Vec<char>>, width: usize, right: usize, down: usize) -> Result<usize> {
+    let mut x = 0;
+    let mut tree_count = 0;
+    for i in 1..input.len() {
+        if i % down != 0 {
+            continue;
+        }
+        x += right;
+        if x > width - 1 {
+            x = x - width;
+        }
+        if input[i][x] == '#' {
+            tree_count += 1;
+        }
+    }
+
+    Ok(tree_count)
+}
+
+fn voyager(path: &str, travses: Vec<(usize, usize)>) -> Result<usize> {
     let input = std::fs::read_to_string(path)?
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<_>>();
 
     let width = input[0].len();
-
-    let mut x = 0;
-    let mut tree_count = 0;
     let mut tree_collection = vec![];
 
     for (right, down) in travses.iter() {
-        for i in 1..input.len() {
-            if i > 0 && i % down != 0 {
-                continue;
-            }
-            x += right;
-            if x > width - 1 {
-                x = x - width;
-            }
-            if input[i][x] == '#' {
-                tree_count += 1;
-            }
-        }
-        tree_collection.push(tree_count);
-        tree_count = 0;
-        x = 0;
+        let count = tree_counter(&input, width, *right, *down)?;
+        tree_collection.push(count);
     }
 
     Ok(tree_collection.iter().fold(1, |acc, v| acc * v))
 }
 
 fn part_one(path: &str) -> Result<usize> {
-    tree_counter(path, vec![(3, 1)])
+    voyager(path, vec![(3, 1)])
 }
 
 fn part_two(path: &str) -> Result<usize> {
-    tree_counter(path, vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
+    voyager(path, vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
 }
 
 fn main() -> Result<()> {
